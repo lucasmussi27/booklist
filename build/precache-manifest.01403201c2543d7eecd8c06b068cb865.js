f5afa373c79b8f19336d14001613ec14ec5a37b9
lucasmussi27 <lucas.mussi27@outlook.com>
2019-07-24 12:53:05 -0300

    Initial commit

---
+++ build/precache-manifest.01403201c2543d7eecd8c06b068cb865.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f24291f71362b39520ad47c37c1b20a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b2877e33cf8311499bc",
+    "url": "/static/css/main.f2fe0a4a.chunk.css"
+  },
+  {
+    "revision": "9445687158d5b88835e3",
+    "url": "/static/js/2.44afff6c.chunk.js"
+  },
+  {
+    "revision": "9b2877e33cf8311499bc",
+    "url": "/static/js/main.5ddc1b7e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
